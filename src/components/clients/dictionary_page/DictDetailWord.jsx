983f7bd76741f6d93bc5c75f367da4f236c2f633
Thang Huynh <thanghuynh0706@gmail.com>
2026-06-02 09:54:21 +0700

    chore: fix file casing for deploying

--- src/components/clients/dictionary_page/DictDetailWord.jsx
+++ src/components/clients/dictionary_page/DictDetailWord.jsx
@@ -1,6 +1,6 @@
 import { Volume2, Bookmark } from "lucide-react";
-import HighlightText from "./HighLightText";
-import { playAudio } from "@/utils/PlayAudio";
+import HighlightText from "./HighlightText";
+import { playAudio } from "@/utils/playAudio";
 
 const DictDetailWord = ({ data }) => {
   if (!data) return null;
